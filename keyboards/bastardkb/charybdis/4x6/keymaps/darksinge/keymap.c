181409afe48247024dd8146e741e35c504950daf
Craig Blackburn <craig.blackburn@youscience.com>
2025-01-24 11:58:03 -0700

    update layout

--- keyboards/bastardkb/charybdis/4x6/keymaps/darksinge/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/darksinge/keymap.c
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#define TAPPING_TERM 225
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 #    include "timer.h"
