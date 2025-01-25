aec90b2113039898babe69c2b7fed2588fea4a86
Craig Blackburn <craig.blackburn@youscience.com>
2025-01-24 22:40:00 -0700

    update layout

--- keyboards/bastardkb/charybdis/4x6/keymaps/darksinge/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/darksinge/keymap.c
@@ -25,6 +25,7 @@ enum charybdis_keymap_layers {
     LAYER_LOWER,
     LAYER_RAISE,
     LAYER_POINTER,
+    LAYER_GAMING,
 };
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
@@ -91,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        POINTER,   ALT_Z,   KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, QK_REP,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_LSFT, LOWER,   KC_ENT,      KC_SPC,  KC_BSPC,
-                                           KC_LCTL, KC_LGUI,       TD(TD_AMETHYST)
+                                           KC_LCTL, KC_LGUI,       AMETHYST
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -103,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        RGB_TOG,  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_TAB,    KC_MINS,  KC_EQL,   KC_GT,   KC_PIPE, KC_TILD, KC_SLSH,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_DEL, KC_PERC, KC_PERC, KC_LBRC, KC_RBRC, QK_GESC,     KC_AMPR, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,
+       KC_DEL, KC_PERC, KC_PERC, KC_LBRC, KC_RBRC, QK_GESC,     KC_AMPR, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, TO(LAYER_GAMING),
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                             C(KC_UP), XXXXXXX, KC_CAPS,    KC_SPC, KC_ESC,
                                            XXXXXXX, XXXXXXX,      KC_ENT
@@ -138,6 +139,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   _______, _______, XXXXXXX,    _______, XXXXXXX,
                                            _______, _______,    XXXXXXX
+  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+
+  [LAYER_GAMING] = LAYOUT(
+  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,   KC_8,   KC_9,   KC_0,    KC_MINS,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_MINS,   KC_A,   KC_S,   KC_D,   KC_F,    KC_G,       KC_H,    KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+       KC_LCTL,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, TOHOME,
+  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                  KC_LSFT, KC_SPC,   KC_ENT,      KC_SPC,  KC_BSPC,
+                                           KC_LALT, KC_LGUI,       AMETHYST
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
